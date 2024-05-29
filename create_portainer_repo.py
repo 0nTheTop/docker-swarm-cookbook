d18438995af540817c9d40b84642edbeec1d8b62
Artur Zdolinski <artur@zdolinski.com>
2024-05-29 01:33:44 +0000

    renamed:    cookbook/stackfiles/node-red/docker-compose.yml -> cookbook/composeStacks/node-red/docker-compose.yml 	new file:   cookbook/composeStacks/qdrant/docker-compose.yml 	renamed:    cookbook/swarm/minio-cluster/docker-stack.yml -> cookbook/swarmStacks/minio-cluster/docker-stack.yml 	renamed:    cookbook/swarm/minio-cluster/info.yaml -> cookbook/swarmStacks/minio-cluster/info.yaml 	renamed:    cookbook/swarm/portainer_agent/docker-stack.yml -> cookbook/swarmStacks/portainer_agent/docker-stack.yml 	renamed:    cookbook/swarm/shepherd/docker-stack.yml -> cookbook/swarmStacks/shepherd/docker-stack.yml 	renamed:    cookbook/swarm/shepherd/info.yaml -> cookbook/swarmStacks/shepherd/info.yaml 	renamed:    cookbook/swarm/traefik/docker-stack.yml -> cookbook/swarmStacks/traefik/docker-stack.yml 	renamed:    cookbook/swarm/traefik/info.yaml -> cookbook/swarmStacks/traefik/info.yaml 	modified:   create_portainer_repo.py 	modified:   templates.json

--- create_portainer_repo.py
+++ create_portainer_repo.py
@@ -21,7 +21,7 @@ class PortainerRepos():
         for url in self.urls:
             r = requests.get(url['url'])
             print(f"Save file: {self.templates_folder_path}/" + url['filename'])
-            with open(self.templates_folder_path + url['filename'], 'w') as f:
+            with open(os.path.join(self.templates_folder_path, url['filename']), 'w') as f:
                 f.write(r.text)
 
     def repos(self) -> List[str]:
@@ -29,7 +29,7 @@ class PortainerRepos():
         for url in self.urls:
             r = {}
             r['name'] = url['source']
-            path = self.templates_folder_path + url['filename']
+            path = os.path.join(self.templates_folder_path, url['filename'])
             # check if file exist
             if os.path.exists(path):
                 r['file'] = path
@@ -47,31 +47,39 @@ class PortainerRepos():
             return data
         
         
-def cookbook_swarms():
+def cookbook(type):
     ret = []
-    folder_path = os.path.join("cookbook", "swarm")
+    if type == 2:
+        cookbook_folder = "swarmStacks"
+        folder_path = os.path.join("cookbook", cookbook_folder)
+        stack_file_name = "docker-stack.yml"
+    if type == 3:
+        cookbook_folder = "composeStacks"
+        folder_path = os.path.join("cookbook", cookbook_folder)
+        stack_file_name = "docker-compose.yml"
+    
     # list all folders
     folders = [f for f in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, f))]
     # check if in each folder exis a info.yaml
-    for folder in folders:
-        path = os.path.join(folder_path, folder, "info.yaml")
+    for project_folder in folders:
+        path = os.path.join(folder_path, project_folder, "info.yaml")
         data = {}
         if os.path.exists(path):
             # read info.yaml
             with open(path, 'r') as f:
                 data = yaml.safe_load(f)
-        # if inside data not exist title... then setup based on folder name
+        # if inside data not exist title... then setup based on project_folder name
         if 'title' not in data:
             # This field must consist of lower-case alphanumeric characters, '_' or '-'
-            data['title'] = '-'.join(word.capitalize() or '_' for word in folder.split('_|-')).replace('_', ' ').replace('-', ' ')
+            data['title'] = '-'.join(word.capitalize() or '_' for word in project_folder.split('_|-')).replace('_', ' ').replace('-', ' ')
             
         if 'repository' not in data:
             data['repository'] = {}
             data['repository']['url'] = "https://github.com/azdolinski/docker-swarm-cookbook"
-            data['repository']['stackfile'] = f"cookbook/swarm/{folder}/docker-stack.yml"
+            data['repository']['stackfile'] = f"cookbook/{cookbook_folder}/{project_folder}/{stack_file_name}"
         
         if 'type' not in data:
-            data['type'] = 2
+            data['type'] = type
 
         if 'platform' not in data:
             data['platform'] = 'linux'
@@ -80,7 +88,7 @@ def cookbook_swarms():
             data['logo'] = ''
             
         if 'description' not in data:
-            data['description'] = folder.replace("_", " ")
+            data['description'] = project_folder.replace("_", " ")
         
         if 'source' in data:
             del data['source']
@@ -88,7 +96,14 @@ def cookbook_swarms():
         data['title'] = f"{data['title']} (Cookbook)"
         ret.append(data)
     return ret
-        
+
+
+def cookbook_swarmStack():
+    return cookbook(2)
+
+def cookbook_composeStack():
+    return cookbook(3)
+    
         
 if __name__ == "__main__":
     featch = PortainerRepos()
@@ -106,8 +121,16 @@ if __name__ == "__main__":
             repo['templates'].append(template)
             id = id + 1
 
-    cbook_swarm = cookbook_swarms()
+    # add cookbooks
+    cbook_swarm = cookbook_swarmStack()
     for cbook in cbook_swarm:
+        cbook['id'] = id
+        repo['templates'].append(cbook)
+        id = id + 1
+
+    # add cookbooks
+    cbook_compose = cookbook_composeStack()
+    for cbook in cbook_compose:
         cbook['id'] = id
         repo['templates'].append(cbook)
         id = id + 1
