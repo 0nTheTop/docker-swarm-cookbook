467850a9c3053a78d30e5f7057cc21f5b118367d
Artur Zdolinski <artur@zdolinski.com>
2024-07-15 21:46:52 +0000

    fix files

---
+++ test_docker_compose.sh
@@ -0,0 +1,34 @@
+#/bin/bash
+
+
+# THis script going to get list of folders which are in the cookbook directory
+# check if each folder/or subfolder there is file named starts from "docker-compose"
+# if file exist - then create array with the name of the folder
+# then run docker-compose config in each of this folders to check if syntax of docker compose is correct
+#
+
+
+# get list of folders in cookbook directory
+cookbooks=()
+for d in cookbook/composeStacks/*; do
+  if [ -d "$d" ]; then
+    cookbooks+=("$d")
+    echo "Starting $d"
+  fi
+done
+
+
+# Check if docker compose didn't return error
+# if return error - then brake script with exit 1
+for path in "${cookbooks[@]}"; do
+    if [ -d "$path" ]; then
+        if [ -f "$path/docker-compose.yml" ]; then
+            echo "--------------------"
+            echo "Exam path: $path"
+            if ! docker-compose  -f "$path/docker-compose.yml" config > /dev/null; then
+                echo "Error: docker-compose config failed in $path"
+                exit 1
+            fi
+        fi
+    fi
+done
